c2230f032ee7fec83baa836b795ce67ac07fa2b5
praizerskater <tomruffcodes@gmail.com>
2019-12-14 21:23:58 -0700

    added styles and all files needed now just need JS code

---
+++ dailyscript.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    // need variables for connecting
+
+    //set up time for if els statement to chek if the user is typing into a box that is past the time of day
+    // save inputs to local storage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
