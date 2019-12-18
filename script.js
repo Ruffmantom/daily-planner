451f60a30fac1efb9a1d1deba1c2a1bfd2c06961
Tom Ruff <ruffmantom@gmail.com>
2019-12-17 18:44:09 -0700

    added js code for a starting point

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     // code below this
     var description = $('.description');
-    var time = moment().hours();
-    console.log(time);
+    // var time = moment().hours();
+    // console.log(time);
 
 
 
@@ -11,18 +11,65 @@ $(document).ready(function () {
     console.log(saved)
 
     $('.saveBtn').on('click', function () {
-        console.log('working')
+        console.log($(this).parent().attr('id')
+        )
+
         if (description) {
             var save = description.val();
             localStorage.setItem('savedInfo', save);
+            // localStorage.setItem('savedInfo' + num, save);
         }
-
-
 
     })
     description.text(saved)
+    // creating function to save to text area
+    function print() {
+        $('.row').each(function () {
+            // create a variable for the id of 'this'
+            console.log($(this).attr('id').split('-')[1])
+
+            // check varible val to the saved info key in local storage
+            //print the saved data
+            // if there isnt data then clear 
+
+
+
+        })
+    }
+    print();
+
+    // function for updating page 
+
+    function hoursUpdater() {
+        var currentHour = moment().hours();
+        $('.time-block').each(function () {
+            // console.log('timeblock is working')// ran 9 times cause there are 9 divs with the class of "time-block"
+            // console.log(this) displays 
+            var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+            console.log(blockHour);
+            if (blockHour < currentHour) {
+                console.log('block hour is before')
+            } else {
+                console.log('to late')
+            }
+
+        })
+
+        console.log('did run');
+
+
+
+
+
+    }
+
+
+
+
     // If we need to apply some CSS, we can quickly do so, using the jQuery ".attr" method.
     //  newDiv.attr("class", "fancy");
+    // description.attr('class', 'past');
 
 
 
@@ -51,8 +98,8 @@ $(document).ready(function () {
 
 
 
-
-
+    hoursUpdater();
+    var interval = setInterval(hoursUpdater, 15000);
 
     // dont code below this
 });
